31ebcb5e27aa2afa047fbb4f91a2f3a1f39bde5d
Atrax7 <frimin.zen@yandex.ru>
2023-04-21 18:27:10 +0300

    added new time function

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+from datetime import datetime as dt
+
+def what_time_is_now():
+    print(dt.now())
+
+
+
+
+
 print('ddddd')
 for i in range(8):
     print(i)
 print(abs(1 - 3))
+
+what_time_is_now()
